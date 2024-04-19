c73ddeb03911a41cecdbfe6074eff5d207c7934a
ridabayi <bayi.rida@gmail.com>
2024-04-19 07:17:16 -0700

    updated 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description: Print single digit
+ *
  * Return: Always 0 (success/corect)
  */
-int main(void)
 
+int main(void)
 {
 	int i;
+
 	for (i = 48 ; i <= 57; i++)
 	{
 		putchar (i);
