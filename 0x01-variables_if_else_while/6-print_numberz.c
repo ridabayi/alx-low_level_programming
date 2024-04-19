d03e5931b1a6dc5b4486fd8e6f746ea97a54ff7a
ridabayi <bayi.rida@gmail.com>
2024-04-19 07:15:18 -0700

    updated

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: Affiche les chiffres de 0 à 9 en utilisant putchar
- *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success/corect)
  */
-
 int main(void)
+
 {
-	int num = 0;
+	int i;
+	for (i = 48 ; i <= 57; i++)
+	{
+		putchar (i);
+	}
+	
+	putchar ('\n');
 
-        while (num <= 9)
-        {
-		putchar(num + 48); // ASCII representation of digits
-                num++;
-        }
+        return (0);
 
-        putchar('\n');
-
-        return 0;
 }
