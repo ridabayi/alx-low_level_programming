9504aabca4787fb5d43bb179c0251cadf500f89d
ridabayi <bayi.rida@gmail.com>
2024-04-19 06:59:27 -0700

    Upd alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 {
-    char letter = 'a';
+	char c = 'a';
 
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
-    putchar('\n');
-
-    return (0);
+	putchar('\n');
+	return (0);
 }
