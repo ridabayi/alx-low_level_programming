0fd3c9f0c23fba30647f3944fe72c4520cd71abb
ridabayi <bayi.rida@gmail.com>
2024-04-19 06:52:53 -0700

    add alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - ENTRY point
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
