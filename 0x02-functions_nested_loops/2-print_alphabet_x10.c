a90b426124cafe123d341899589313d38ed199e3
ridabayi <bayi.rida@gmail.com>
2024-04-23 13:08:47 -0700

    up 2 script

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
- *
- * print_alphabet_x10 - Prints the alphabet in lowercase
- *
- * Return: Always 0
- * 
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
-
 void print_alphabet_x10(void)
 {
-    int line_count;
-    char letter;
+	int count = 0;
+	char alphabet;
 
-    for (line_count = 0; line_count < 10; line_count++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	while (count++ <= 9)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+		_putchar('\n');
+	}
 }
