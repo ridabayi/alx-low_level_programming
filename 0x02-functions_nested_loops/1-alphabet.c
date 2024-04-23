d22680ec74da9d319e2be012aec609b4a372b6b1
ridabayi <bayi.rida@gmail.com>
2024-04-23 12:51:18 -0700

    Upda alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * Print_alphabet - Prints the alphabet in lowercase followed by a newline
  *
- * Description: This function prints the lowercase alphabet characters from 'a' to 'z', each followed by a newline character.
+ * Description: This function prints the lowercase alphabet characters
  *
  * Return: Always 0
  */
