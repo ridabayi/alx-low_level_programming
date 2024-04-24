26c8e2c6370af02d78df89771ae67a074afa4bb0
ridabayi <bayi.rida@gmail.com>
2024-04-24 04:18:36 -0700

    add 120

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    long int first = 1, second = 2, next;
+
+    printf("%ld, %ld", first, second);
+
+    for (i = 3; i <= 50; ++i)
+    {
+        next = first + second;
+        printf(", %ld", next);
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
