735fd0a5365b424e85728a428b1acdd4f7ac508d
ridabayi <bayi.rida@gmail.com>
2024-04-24 05:40:11 -0700

    updat 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the first 98 Fibonacci numbers
+ * main - Finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int count;
-    unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long int fib1 = 1, fib2 = 2, next;
 
-    for (count = 0; count < 97; count++)
-    {
-        n3 = n1 + n2;
-        printf("%lu, ", n3);
-        n1 = n2;
-        n2 = n3;
-    }
+	printf("%lu", fib1);
 
-    n3 = n1 + n2;
-    printf("%lu\n", n3);
+	for (int i = 1; i < 91; i++)
+	{
+		printf(", %lu", fib2);
+		next = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = next;
+	}
 
-    return (0);
+	unsigned long int fib1_high = fib1 / 1000000000;
+	unsigned long int fib1_low = fib1 % 1000000000;
+	unsigned long int fib2_high = fib2 / 1000000000;
+	unsigned long int fib2_low = fib2 % 1000000000;
+
+	for (int i = 92; i < 99; ++i)
+	{
+		printf(", %lu", fib1_high + (fib2_high / 1000000000));
+		printf("%lu", fib2_low);
+		fib1_high += fib2_high;
+		fib2_high = fib1_high - fib2_high;
+		fib2_low += fib1_low;
+		fib1_low = fib2_low - fib1_low;
+	}
+
+	printf("\n");
+
+	return (0);
 }
