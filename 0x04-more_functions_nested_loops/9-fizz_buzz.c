6e9a017f891735ccf958234e6584455ec2a51c36
ridabayi <bayi.rida@gmail.com>
2024-04-26 03:45:09 -0700

    add 9sc

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,50 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * fizzbuzz - Prints the FizzBuzz sequence up to a given number.
- * @n: The number up to which the FizzBuzz sequence should be printed.
- *
- * Return: void
- */
-void fizzbuzz(int n)
-{
-    int i;
-
-    for (i = 1; i <= n; i++)
-    {
-        switch (i % 15)
-        {
-            case 0:
-                _putchar("FizzBuzz ");
-                break;
-            case 3:
-            case 6:
-            case 9:
-            case 12:
-                _putchar("Fizz ");
-                break;
-            case 5:
-            case 10:
-                _putchar("Buzz ");
-                break;
-            default:
-                _putchar(i + '0');
-                if (i != n)
-                    _putchar(' ');
-                break;
-        }
-    }
-    _putchar('\n');
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
 {
-    fizzbuzz(100);
-
-    return (0);
+    int i;
+    
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
+        
+        if (i < 100)
+            printf(" ");
+        else
+            printf("\n");
+    }
+    
+    return 0;
 }
