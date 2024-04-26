8f3f0c99dcbf0e53f0882648d8ba1852684fc42d
ridabayi <bayi.rida@gmail.com>
2024-04-26 03:47:34 -0700

    add 9scri

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,31 +3,41 @@
 /**
  * main - Entry point
  *
- * Description: Prints the numbers from 1 to 100, replacing multiples of
+ * Description: Prints numbers from 1 to 100, replacing multiples of
  *              3 with Fizz, multiples of 5 with Buzz, and multiples of both
  *              3 and 5 with FizzBuzz.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
+    int num = 1;
 
-    for (i = 1; i <= 100; i++)
+    while (num <= 100)
     {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
+        if (num % 3 == 0 && num % 5 == 0)
+        {
+            _printf("%s", "FizzBuzz");
+        }
+        else if (num % 3 == 0)
+        {
+            _printf("%s", "Fizz");
+        }
+        else if (num % 5 == 0)
+        {
+            _printf("%s", "Buzz");
+        }
         else
-            printf("%d", i);
+        {
+            _printf("%d", num);
+        }
 
-        if (i < 100)
-            printf(" ");
-        else
-            printf("\n");
+        if (num != 100)
+        {
+            _printf(" ");
+        }
+        num++;
     }
 
+    _printf("\n");
     return (0);
 }
