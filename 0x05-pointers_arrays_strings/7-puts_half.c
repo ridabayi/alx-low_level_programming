727b8adf741deaef20b926d95a6e7e98937479c2
ridabayi <bayi.rida@gmail.com>
2024-04-28 05:54:03 -0700

    add updat 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * puts_half - Prints the second half of a string.
+ * @str: Pointer to the string.
  *
- * Return: Always 0
+ * Return: void.
  */
-int main(void)
+void puts_half(char *str)
 {
-	char *str;
-	int size = 11; /* Including the null terminator */
+	int length = 0;
+	int i = 0;
 
-	str = malloc(size * sizeof(char));
-	if (str == NULL)
-		return (1);
+	while (str[length] != '\0')
+		length++;
 
-	str = "0123456789";
-	puts_half(str);
+	if (length % 2 == 0)
+		i = length / 2;
+	else
+		i = (length + 1) / 2;
 
-	free(str);
-	return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
