35765fe255ee163fb34e55669b01b0e63b2409be
ridabayi <bayi.rida@gmail.com>
2024-05-06 04:01:22 -0700

    add 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @str: The input string
+ *
+ * Return: The modified string
+ */
+
+char *leet(char *str)
+{
+    int i = 0;
+    int j = 0;
+
+    while (str[i]!= '\0')
+    {
+        if (str[i] == 'a' || str[i] == 'A')
+            str[i] = '4';
+        else if (str[i] == 'e' || str[i] == 'E')
+            str[i] = '3';
+        else if (str[i] == 'o' || str[i] == 'O')
+            str[i] = '0';
+        else if (str[i] == 't' || str[i] == 'T')
+            str[i] = '7';
+        else if (str[i] == 'l' || str[i] == 'L')
+            str[i] = '1';
+
+        i++;
+    }
+
+    return (str);
+}
