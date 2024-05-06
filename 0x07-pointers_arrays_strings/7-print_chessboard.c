819e569da4c99b29f272ecd395803caea1bb625d
ridabayi <bayi.rida@gmail.com>
2024-05-06 04:34:16 -0700

    add update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
- * print_chessboard -  function that prints the chessboard.
- * @a: 2D Array
- * Return: the number of bytes in the initial segment of s
- * On error, -1 is returned, and errno is set appropriately.
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: no return.
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
-	int j = 0;
+	unsigned int i, m = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 64; i++)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[i][j]);
+			m = i;
+			_putchar('\n');
 		}
-		_putchar('\n');
+		_putchar(a[i / 8][i - m]);
 	}
+	_putchar('\n');
 }
