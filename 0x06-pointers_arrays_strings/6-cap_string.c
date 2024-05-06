f966d635f42f71bfccfa58983a18390919da93c1
ridabayi <bayi.rida@gmail.com>
2024-05-06 03:58:26 -0700

    add 6 update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,28 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * cap_string - Capitalizes all words of a string
+ * @string: The input string
  *
- * @str: The input string
- *
- * Return: The modified string
+ * Return: The input string
  */
-char *cap_string(char *str)
+
+char *cap_string(char *string)
 {
-    int i = 0;
-    int capitalize = 1;
+	const char OFFSET = 'a' - 'A';
+	int j, i = 1;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-    while (str[i]!= '\0')
-    {
-        if (capitalize && str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
-
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-            capitalize = 1;
-        else
-            capitalize = 0;
-
-        i++;
-    }
-
-    return (str);
+	i = 1;
+	if (string[0] >= 'a' && string[0] <= 'z')
+		string[0] -= OFFSET;
+	while (string[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+			if (string[i - 1] == sep[j] && (string[i] >= 'a' && string[i] <= 'z'))
+				string[i] -= OFFSET;
+		i++;
+	}
+	return (string);
 }
