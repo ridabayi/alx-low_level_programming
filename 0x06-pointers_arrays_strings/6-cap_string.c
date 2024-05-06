8ec52fdad2ffa336ec84e9a47b85b6481a762361
ridabayi <bayi.rida@gmail.com>
2024-05-06 03:45:12 -0700

    'add 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,37 +2,38 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * @str: The string to capitalize.
  *
- * Return: A pointer to the changed string.
+ * Return: Pointer to the resulting string.
  */
 char *cap_string(char *str)
 {
-    int index = 0;
+    int i = 0;
 
-    while (str[index])
+    while (str[i] != '\0')
     {
-        while (!(str[index] >= 'a' && str[index] <= 'z'))
-            index++;
-
-        if (str[index - 1] == ' ' ||
-            str[index - 1] == '\t' ||
-            str[index - 1] == '\n' ||
-            str[index - 1] == ',' ||
-            str[index - 1] == ';' ||
-            str[index - 1] == '.' ||
-            str[index - 1] == '!' ||
-            str[index - 1] == '?' ||
-            str[index - 1] == '"' ||
-            str[index - 1] == '(' ||
-            str[index - 1] == ')' ||
-            str[index - 1] == '{' ||
-            str[index - 1] == '}' ||
-            index == 0)
-            str[index] -= 32;
-
-        index++;
+        /* Check if the current character is a separator */
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+            str[i] == '}')
+        {
+            /* Move to the next character */
+            i++;
+            /* Check if the next character is a lowercase letter */
+            if (str[i] >= 'a' && str[i] <= 'z')
+            {
+                /* Convert the letter to uppercase */
+                str[i] -= 32;
+            }
+        }
+        else
+        {
+            /* Move to the next character */
+            i++;
+        }
     }
 
-    return (str);
+    return str;
 }
